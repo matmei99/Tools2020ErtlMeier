4189b054ec3e8f8df32038890c932ec21e31268b
Richy <Richard.Ertl@gmx.net>
2020-05-07 15:09:21 +0200

    clean

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 
+public void thisIsANewMethod() {
+
+	
+}
 
 
 int main (int argc, char *argv[]) 
 { 
-	public void thisIsANewMethod() {
-
-		bool nice = true;
 	
-
-		return nice;
-	}
 
 	return 0;
 } 
