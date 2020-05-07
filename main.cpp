c6ab5fccacbf33491ebfba2770968133ce776f78
S1910824006 (Meier Matthias) <s1910824006@students.fh-hagenberg.at>
2020-05-07 15:39:52 +0200

    Added Hello World

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-
-public void thisIsANewMethod() {
-	std::cout<< "Hello World";
-	bool nice = true;
+public void thisIsAnotherNewMethod() {
+	std::cout << "HelloWorld";
+	std::cout << "HelloWorld";
+	std::cout << "HelloWorld";
+	bool nice = false;
 }
-
-
-int main (int argc, char *argv[]) 
-{ 
+int main (int argc, char *argv[])
+{
 	return 0;
-} 
-
+}
